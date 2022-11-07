268b48c2f009fe135248c5d5fe7df41102020555
PiRed0 <96204399+PiRed0@users.noreply.github.com>
2022-11-07 16:50:22 +0100

    Update MesuresPdfManuscrits.py

--- MesuresPdfManuscrits.py
+++ MesuresPdfManuscrits.py
@@ -1,12 +1,12 @@
 # Conversion de mesures réalisées sur un pdf de manuscrit
 
-hauteurRelle = input('Hauteur réelle du manuscrit = ')
-hauteurPDF = input('Hauteur du manuscrit sur le pdf = ')
+hauteur_relle: float = input('Hauteur réelle du manuscrit = ')
+hauteur_pdf: float = input('Hauteur du manuscrit sur le pdf = ')
 
-mesure = 'vide'
+mesure: str = 'vide'
 
 while mesure != '!':
     mesure = input('Mesure sur le pdf = ')
-    resultat = float(mesure) * float(hauteurRelle) / float(hauteurPDF)
-    print('Mesure réelle = ' + str(round(resultat, 1)))
+    resultat: float = float(mesure) * hauteur_relle / floathauteur_pdf
+    print('Mesure réelle = ',round(resultat, 1))
 print('Fin')
